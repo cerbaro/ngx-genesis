5bedb83f72e289f96908d9d234900910a22d1038
Vinícius Andrei Cerbaro <contato@cerbaro.net>
2018-07-12 08:55:42 -0400

    working on tabs

--- src/app/pages/field/tabs/climate/climate.module.ts
+++ src/app/pages/field/tabs/climate/climate.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClimateComponent } from './climate/climate.component';
 import { ClimateRoutingModule } from './climate-routing.module';
+import { LayoutComponentsModule } from 'src/app/shared/modules/layout-components.module';
 
 @NgModule({
     imports: [
         CommonModule,
 
-        ClimateRoutingModule
+        ClimateRoutingModule,
+
+        LayoutComponentsModule
     ],
     declarations: [
         ClimateComponent
--- src/app/pages/field/tabs/phenology/phenology.module.ts
+++ src/app/pages/field/tabs/phenology/phenology.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhenologyComponent } from './phenology/phenology.component';
 import { PhenologyRoutingModule } from './phenology-routing.module';
+import { LayoutComponentsModule } from 'src/app/shared/modules/layout-components.module';
 
 @NgModule({
     imports: [
         CommonModule,
 
-        PhenologyRoutingModule
+        PhenologyRoutingModule,
+
+        LayoutComponentsModule
     ],
     declarations: [
         PhenologyComponent
